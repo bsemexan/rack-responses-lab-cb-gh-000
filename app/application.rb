c09f3f067d45bb16cb6a8f0cc27abd2e1375609b
Berry  Semexan <bsemexan@gmail.com>
2018-03-02 22:51:01 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,12 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Good Morning!"
-    resp.write "Good Afternoon!"
+    t = Time.now
+    if t.hour <= 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
   end
 
 end
